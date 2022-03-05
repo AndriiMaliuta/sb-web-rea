20dbf7a769fc1efedc1e6fc30ad12b3c971d2964
andrii <quadr988@gmail.com>
2022-03-05 20:22:00 +0200

    init

--- src/main/java/com/anma/sb/sbwebrea/DogController.java
+++ src/main/java/com/anma/sb/sbwebrea/DogController.java
@@ -2,12 +2,13 @@ package com.anma.sb.sbwebrea;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 
 import java.util.ArrayList;
 import java.util.List;
 
-@Controller
+@RestController
 public class DogController {
 
     @GetMapping("/dogs")
@@ -17,13 +18,14 @@ public class DogController {
 
         return Flux.create((fluxSink) -> {
             int counter = 0;
-            String res = "";
+            String res = "[";
             do {
-                fluxSink.next("added " + counter);
+                fluxSink.next("{ \"value\": \"added " + counter + "\" },");
                 counter++;
 //                fluxSink.onRequest((r) -> System.out.println("Request initiated")).next(catBreed);
             } while (counter < 30);
             {
+//                fluxSink.next("]");
                 fluxSink.complete();
             }
         });
